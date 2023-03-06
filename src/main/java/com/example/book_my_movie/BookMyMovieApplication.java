452a648a4c4e5431bf577d1df3ceacf3af727e0e
Satish <satishkrgupta555@gmail.com>
2023-03-06 20:16:24 +0530

    bmm v0.01 - packages created

---
+++ src/main/java/com/example/book_my_movie/BookMyMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_my_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book_my_movie/BookMyMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_my_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
